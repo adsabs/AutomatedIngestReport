d6e4d99cfa9c943bdb8ddd71ed431c4b2c090055
SpacemanSteve <Stephen.McDonald@cfa.harvard.edu>
2018-10-02 20:55:49 -0400

    forgot to return status

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 
 # where to send solr queries, includes core name
-SOLR_URL = 'http://localhost:19984/solr/collection1/'
+# SOLR_URL = 'http://localhost:19984/solr/collection1/'
+SOLR_URL = 'http://localhost:9983/solr/collection1/'
 
 # the current list of canonical bibcodes
 CANONICAL_FILE = '/proj/ads/abstracts/config/bibcodes.list.can'
 
+ELASTICSEARCH_URL = 'https://search-pipeline-d6gsitdlgp2dh25slmlrcwjtse.us-east-1.es.amazonaws.com'
+
+SQLALCHEMY_URL_MASTER = 'postgres://master_pipeline:master_pipeline@%s:15432/master_pipeline' % 'adsqb.cfa.harvard.edu'
+SQLALCHEMY_URL_NONBIB = 'postgres://data_pipeline:data_pipeline@%s:15432/data_pipeline' % 'adsqb.cfa.harvard.edu'
 
 #AIR_DIRECTORY = '/Users/SpacemanSteve/code/eclipse/workspace/air/air/tests/stubdata/'
 # home of data files (e.g. a copy of today's canonical bibcodes)
