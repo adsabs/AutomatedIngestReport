6263f7acea209efb3254bada2f6c697b4f3ebc85
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2021-05-16 08:37:38 -0400

    modified:   air/compute.py  	modified:   run.py

--- air/compute.py
+++ air/compute.py
@@ -20,6 +20,7 @@ class Compute(object):
     def canonical(self):
         up = GoogleUploader()
         url_string = 'https://drive.google.com/file/d/'
+        fold_id = conf.get('DATA_FOLDER',None)
 
         """compute new, deleted"""
         canonical_start = Filename.get(self.start, FileType.CANONICAL)
@@ -27,12 +28,22 @@ class Compute(object):
         canonical_new = Filename.get(self.end, FileType.CANONICAL, FileAdjective.NEW)
         self.values['new_canonical'] = comm(canonical_end, canonical_start, canonical_new)
         if os.path.exists(canonical_new):
-            self.values['new_canonical_file'] = url_string + up.upload_file(infile=canonical_new, folderID=conf.get('DATA_FOLDER',None))
+            try:
+                fileid = up.upload_file(infile=canonical_new, folderID=fold_id)
+                new_canonical_file = url_string + fileid
+                self.values['new_canonical_file'] = new_canonical_file
+            except Exception as err:
+                print("Err in compute: %s" % err)
 
         canonical_deleted = Filename.get(self.end, FileType.CANONICAL, FileAdjective.DELETED)
         self.values['deleted_canonical'] = comm(canonical_start, canonical_end, canonical_deleted)
         if os.path.exists(canonical_deleted):
-            self.values['deleted_canonical_file'] = url_string + up.upload_file(infile=canonical_deleted, folderID=conf.get('DATA_FOLDER',None))
+            try:
+                fileid = up.upload_file(infile=canonical_deleted, folderID=fold_id)
+                deleted_canonical_file = url_string + fileid
+                self.values['deleted_canonical_file'] = deleted_canonical_file
+            except Exception as err:
+                print("Err in compute: %s" % err)
 
         self.values['canonical'] = lines_in_file(canonical_end)
 
@@ -43,18 +54,33 @@ class Compute(object):
         solr_missing = Filename.get(self.end, FileType.SOLR, FileAdjective.MISSING)
         self.values['missing_solr'] = comm(canonical_end, solr_end, solr_missing)
         if os.path.exists(solr_missing):
-            self.values['missing_solr_file'] = url_string + up.upload_file(infile=solr_missing, folderID=conf.get('DATA_FOLDER',None))
+            try:
+                fileid = up.upload_file(infile=solr_missing, folderID=fold_id)
+                missing_solr_file = url_string + fileid
+                self.values['missing_solr_file'] = missing_solr_file
+            except Exception as err:
+                print("Err in compute: %s" % err)
 
         solr_start = Filename.get(self.start, FileType.SOLR)
         solr_new = Filename.get(self.end, FileType.SOLR, FileAdjective.NEW)
         self.values['new_solr'] = comm(solr_end, solr_start, solr_new)
         if os.path.exists(solr_new):
-            self.values['new_solr_file'] = url_string + up.upload_file(infile=solr_new, folderID=conf.get('DATA_FOLDER',None))
+            try:
+                fileid = up.upload_file(infile=solr_new, folderID=fold_id)
+                new_solr_file = url_string + fileid
+                self.values['new_solr_file'] = new_solr_file
+            except Exception as err:
+                print("Err in compute: %s" % err)
 
         solr_deleted = Filename.get(self.end, FileType.SOLR, FileAdjective.DELETED)
         self.values['deleted_solr'] = comm(solr_start, solr_end, solr_deleted)
         if os.path.exists(solr_deleted):
-            self.values['deleted_solr_file'] = url_string + up.upload_file(infile=solr_deleted, folderID=conf.get('DATA_FOLDER',None))
+            try:
+                fileid = up.upload_file(infile=solr_deleted, folderID=fold_id)
+                deleted_solr_file = url_string + fileid
+                self.values['deleted_solr_file'] = deleted_solr_file
+            except Exception as err:
+                print("Err in compute: %s" % err)
 
         solr_extra = Filename.get(self.end, FileType.SOLR, FileAdjective.EXTRA)
         self.values['extra_solr'] = comm(solr_end, canonical_end, solr_extra)
